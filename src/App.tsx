7ac41d9c09ed338e366b92798a24d193703a0fce
enbraining <lead@xolving.com>
2023-11-22 16:01:24 +0900

    update icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Header } from './components/Header'
 import desktop from '/desktop_tutorics.svg'
 
 function App() {
   return (
     <>
-      {/* <Header/>*/}
+      <div>
+        <Header/>
+      </div>
       <img src={desktop} className="desktop_image" alt="desktop_image" />
       <h3 className='h3'><a className='point'>tutorics</a> coming soon</h3>
     </>
